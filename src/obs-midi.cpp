469d84c59a6dbe6c119bdbca4447cf9734847df0
Christopher P. Yarger <cpyarger@gmail.com>
2022-02-21 20:41:22 -0500

    Remove autoremove dataypes
    For compatability with latest obs studio

--- src/obs-midi.cpp
+++ src/obs-midi.cpp
@@ -21,12 +21,6 @@
 #include "events.h"
 using namespace std;
 
-void ___source_dummy_addref(obs_source_t *) {}
-void ___sceneitem_dummy_addref(obs_sceneitem_t *) {}
-void ___data_dummy_addref(obs_data_t *) {}
-void ___data_array_dummy_addref(obs_data_array_t *) {}
-void ___output_dummy_addref(obs_output_t *) {}
-
 void ___data_item_dummy_addref(obs_data_item_t *) {}
 void ___data_item_release(obs_data_item_t *dataItem)
 {
